43cfa75ff8da80a368c175b3a531f0e92460dde6
YosukeSUGIURA <44398746+YosukeSugiura@users.noreply.github.com>
2022-10-04 20:38:58 +0900

    revise

---
+++ 06_read_write/06_read_write_2Darray.py
@@ -0,0 +1,18 @@
+"""
+	演習課題： ２次元データの取り扱い
+"""
+import pandas as pd
+import numpy as np
+
+# ファイルの読み込み
+input_file = 'iris.csv'	# 読み込むファイル名
+input_data = pd.read_csv(input_file)
+input_data = np.array(input_data)
+
+# 「variety」以外の要素を配列output_dataに入れる．．
+output_data = []
+for row in input_data:
+	output_data.append(row[0:4])
+output_data = np.array(output_data) # おまじない
+
+print(output_data)
--- 06_read_write/06_read_write_csv.py
+++ 06_read_write/06_read_write_csv.py
@@ -1,11 +1,12 @@
 """
 	練習課題： csvファイルの読み書き
 """
-
-# 先頭行を読み込む
+import pandas as pd
 import numpy as np
 
+# ファイルの読み込み
 input_file = 'iris.csv'	# 読み込むファイル名
-input_data = np.loadtxt(input_file, delimiter=',', skiprows=0)
+input_data = pd.read_csv(input_file)
+input_data = np.array(input_data)
 
 print(input_data)
