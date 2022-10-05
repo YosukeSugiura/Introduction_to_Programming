2be3a0f22111c6783a5133aeed8f6a392583b1ac
YosukeSUGIURA <44398746+YosukeSugiura@users.noreply.github.com>
2022-10-05 19:23:20 +0900

    revised

--- 07_module/07_module_ex1.py
+++ 07_module/07_module_ex1.py
@@ -1,18 +1,35 @@
 """
-	練習問題：関数の使い方
+	練習問題：関数の定義
 """
+# 定義した関数を使う例
+import pandas as pd
+import numpy as np
 
-import 
+# 関数：ファイルを読み込む
+def read_file(file_name):
+    input_data = pd.read_csv(file_name)
+    input_data = np.array(input_data)
+    return input_data
 
-# 関数の定義
-def print_
+# 関数：ファイルを書き出す
+# 	引数1：書き込むデータ，引数2：ファイル名
+def write_file(data, output_file):
+	# ファイルの書き込みを行う処理をここに書く
+	return 0
 
+# ファイルの読み込み
+input_data1 = read_file('input1.csv')
+input_data2 = read_file('input2.csv')
+input_data3 = read_file('input3.csv')
 
-# リストの作成
-List = [10, 'ハト', 1, 0.1, '栗', '1001']
+# 読み込んだデータの表示
+print(input_data1)
+print(input_data2)
+print(input_data3)
 
-# リストから数値だけを表示する
-for item in List:
-	if str.isdigit(item) == 1:
-		
+# 書き込むファイルの名前
+output_file1 = 'output1.csv'
+output_file2 = 'output2.csv'
+output_file3 = 'output3.csv'
 
+# ファイルの書き込み
