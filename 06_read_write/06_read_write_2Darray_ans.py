b02b45cba09d5c323a0d6f3f893209e55e1ac9b3
YosukeSUGIURA <44398746+YosukeSugiura@users.noreply.github.com>
2022-10-17 15:04:45 +0900

    Add files via upload

--- 06_read_write/06_read_write_2Darray_ans.py
+++ 06_read_write/06_read_write_2Darray_ans.py
@@ -1,15 +1,4 @@
 """
-練習課題： csvファイルの読み書き
-"""
-import pandas as pd
-import numpy as np
-
-# ファイルの読み込み
-input_file = 'iris.csv'	# 読み込むファイル名
-input_data = pd.read_csv(input_file)
-input_data = np.array(input_data)
-
-"""
 	演習課題： ２次元データの取り扱い
 """
 import pandas as pd
