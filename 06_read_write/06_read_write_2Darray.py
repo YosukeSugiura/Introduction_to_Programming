86251c4e9134620f6d1b7c04ad69c8d0ea9de96b
YosukeSUGIURA <44398746+YosukeSugiura@users.noreply.github.com>
2022-10-17 14:54:03 +0900

    Update 06_read_write_2Darray.py

--- 06_read_write/06_read_write_2Darray.py
+++ 06_read_write/06_read_write_2Darray.py
@@ -11,7 +11,7 @@ input_data = np.array(input_data)
 
 # 「variety」以外の要素を配列output_dataに入れる．．
 output_data = []
-for row in input_data:
+for row in input_data:	# １行ずつ取り出して row に入れる．
 	output_data.append(row[0:4])
 output_data = np.array(output_data) # おまじない
 
